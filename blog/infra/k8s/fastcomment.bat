04724943cd841d89ce8e3ddfd2934bb2b22936ef
LinnLattOo <linnlattoo@gusto.edu.mm>
2022-02-17 03:44:26 +0630

    command error

--- blog/infra/k8s/fastcomment.bat
+++ blog/infra/k8s/fastcomment.bat
@@ -1,7 +1,10 @@
-kubectl apply -f event-bus-depl.yaml -f comments-depl.yaml -f comments-srv.yaml -f moderation-depl.yaml -f posts-depl.yaml -f posts-srv.yaml -f query-depl.yaml 
+kubectl apply -f event-bus-depl.yaml -f comments-depl.yaml -f comments-srv.yaml -f moderation-depl.yaml -f posts-depl.yaml -f posts-srv.yaml -f query-depl.yaml -f client-depl.yaml -f ingress-srv.yaml 
+kubectl apply -f event-bus-depl.yaml -f comments-depl.yaml -f moderation-depl.yaml -f posts-depl.yaml-f query-depl.yaml -f client-depl.yaml -f ingress-srv.yaml 
 
-kubectl delete developments comments-depl event-bus-depl moderation-depl posts-depl query-depl
+kubectl delete deployments comments-depl event-bus-depl moderation-depl posts-depl query-depl
+kubectl delete deployments comments-depl event-bus-depl moderation-depl posts-depl query-depl client-depl
 
 ingress-nginx 
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
