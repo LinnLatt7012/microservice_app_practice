b7baa6df054ded15267e4d4b0d8043ed48f2f8c4
LinnLattOo <linnlattoo@gusto.edu.mm>
2022-02-17 23:45:30 +0630

    skaffold file added

--- blog/fastcomment.bat
+++ blog/fastcomment.bat
@@ -4,7 +4,12 @@ kubectl apply -f event-bus-depl.yaml -f comments-depl.yaml -f moderation-depl.ya
 kubectl delete deployments comments-depl event-bus-depl moderation-depl posts-depl query-depl
 kubectl delete deployments comments-depl event-bus-depl moderation-depl posts-depl query-depl client-depl
 
+kubectl rollout restart deployment posts-depl 
+
 ingress-nginx 
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
 kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+kubectl get service ingress-nginx-controller --namespace=ingress-nginx 
+
+skaffold
